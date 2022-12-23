bd5a9408bf185fd6b9bd93f3a3e2546df110e6e7
MudassirKz <98865127+Mudassirkz@users.noreply.github.com>
2022-12-23 09:50:01 +0500

    some changes.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
+import { useEffect } from "react";
 import "./App.css";
 import { capitalizeFirstLetter } from "./common/capitalizeFirstLetter";
 
 function App() {
-  let arr = [];
-  ["R>A", "F>R", "A>N", "C>E", "N>C"].map((item) => {
-    if (item[0] < item[2]) {
-      arr.push(item[0]);
-      arr.push(item[2]);
-    }
-  });
-  let result = capitalizeFirstLetter(arr.join("").toLowerCase());
+  const getResult = (array) => {
+    let arr = [];
+    array.map((item) => {
+      if (item[0] < item[2]) {
+        arr.push(item[0]);
+        arr.push(item[2]);
+      }
+    });
+    let result = capitalizeFirstLetter(arr.join("").toLowerCase());
+    return result;
+  };
+  useEffect(() => {
+    let res = getResult(["R>A", "F>R", "A>N", "C>E", "N>C"]);
+    console.log("res", res);
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <p>{result}</p>
-      </div>
+      <div>{/* <p>{res}</p> */}</div>
     </div>
   );
 }
