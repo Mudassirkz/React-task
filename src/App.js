0a2457553fe5c3c0ae5a535ca4cc874c87e1179d
MudassirKz <98865127+Mudassirkz@users.noreply.github.com>
2023-01-10 12:46:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { capitalizeFirstLetter } from "./common/capitalizeFirstLetter";
+import { Button } from "antd";
+import { ChildComponent } from "./common/component/ChildComponent";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   const getResult = (array) => {
     let arr = [];
     array.map((item) => {
@@ -16,11 +20,36 @@ function App() {
   };
   useEffect(() => {
     let res = getResult(["R>A", "F>R", "A>N", "C>E", "N>C"]);
-    console.log("res", res);
+    // console.log("res", res);
   }, []);
 
-  return null;
-  // return <div className="App"><div><p>{res}</p></div></div>;
+  // useCallback exampe for function memoization function only run when function call don'y render child component every time.
+  const handleUpdateNum = useCallback(() => {
+    console.log("clallback renders");
+  }, []);
+
+  // use memo stop rendering every when parent component is render it's return a memoized value
+  const getChildComp = useMemo(
+    () => <ChildComponent handleUpdateNum={handleUpdateNum} />,
+    [handleUpdateNum]
+  );
+
+  return (
+    <div className="App">
+      <div>
+        <p>{count}</p>
+        {getChildComp}
+        <p></p>
+        <Button
+          onClick={() => {
+            setCount((prevState) => prevState + 1);
+          }}
+        >
+          Addition
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
